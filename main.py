d5f432df8a1ba06a05ad80e42b40ffe8b01fc61b
Kamaboko0126 <maiwa0126@gmail.com>
2023-09-11 10:33:40 +0800

    20230911

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware 
 import json
 import uuid
+from mmaction.apis import inference_recognizer, init_recognizer
 
 from experiment.frames import calculate_folder_frame_count
 
@@ -45,7 +46,17 @@ async def create_upload_file(file: UploadFile = File(...)):
     with open(f"{VIDEODIR}{file.filename}", "wb") as f:
         f.write(contents)
 
-    return {"filename": file.filename}
+    config_path = 'configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-100e_kinetics400-rgb.py'
+    checkpoint_path = 'bmn_2xb8-400x100-9e_activitynet-feature_20220908-79f92857.pth' # 可以是本地路径
+    img_path = '/upload/' + file.filename   # 您可以指定自己的图片路径
+
+    # 从配置文件和权重文件中构建模型
+    model = init_recognizer(config_path, checkpoint_path, device="cuda:0")  # device 可以是 'cuda:0'
+    # 对单个视频进行测试
+    result = inference_recognizer(model, img_path)
+
+
+    return result
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
