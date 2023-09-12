af0344ab5cac0a666c76b6c2310fa2ad2a08c58a
Maiwawa <88273453+Maiwawa@users.noreply.github.com>
2023-09-12 17:44:47 +0800

    20230912

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware 
 import json
 import uuid
-from mmaction.apis import inference_recognizer, init_recognizer
 
+from mmaction.apis import inference_recognizer, init_recognizer
 from experiment.frames import calculate_folder_frame_count
 
 app = FastAPI()
@@ -39,24 +39,26 @@ app.add_middleware(
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile = File(...)):
 
-    file.filename = f"{uuid.uuid4()}.mp4"
+    file.filename = f"{uuid.uuid4()}.jpg"
     contents = await file.read()
 
     #save the file
     with open(f"{VIDEODIR}{file.filename}", "wb") as f:
         f.write(contents)
 
-    config_path = 'configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x8-100e_kinetics400-rgb.py'
+    config_path = './configs/bmn_2xb8-400x100-9e_activitynet-feature.py'
     checkpoint_path = 'bmn_2xb8-400x100-9e_activitynet-feature_20220908-79f92857.pth' # 可以是本地路径
-    img_path = '/upload/' + file.filename   # 您可以指定自己的图片路径
+    img_path = '/videos/' + file.filename   # 您可以指定自己的图片路径
 
     # 从配置文件和权重文件中构建模型
     model = init_recognizer(config_path, checkpoint_path, device="cuda:0")  # device 可以是 'cuda:0'
     # 对单个视频进行测试
+    #inference_recognizer() 跑不起來
     result = inference_recognizer(model, img_path)
 
+    return {"filename": img_path}
 
-    return result
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
